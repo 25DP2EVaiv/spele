91b80d5fcaee1d2d68c9459bcc52801f2415d1a4
EMVaivods <mihaelsvaivods@gmail.com>
2026-05-26 22:54:51 +0300

    pievienoju + kopejo energijas skaititaju un sanu paneli

--- script.js
+++ script.js
@@ -7,3 +7,54 @@ setInterval(() => {
     document.getElementById('saule').src = x + "_saules_kadrs.png";
 }, 100);
 
+const sanu_verejs = document.getElementById('sanu_verejs')
+const neredzama_dala = document.getElementById('neredzama_dala')
+
+sanu_verejs.addEventListener("click", () => {
+    if (neredzama_dala.style.right === '0px') {
+        neredzama_dala.style.right = '-300px';
+        sanu_verejs.style.right = '0px';
+    } 
+    else {
+        neredzama_dala.style.right = '0px';
+        sanu_verejs.style.right = '300px'
+    }
+});
+
+const skaititajs = document.getElementById('skaititajs')
+let elektriba = 0
+
+setInterval(() => {
+    elektriba += 1;
+    skaititajs.textContent = 'Elektrība = ' + elektriba;
+}, 1000);
+
+const saule = document.getElementById('saule')
+const tekts = document.getElementById('teksts')
+let klikskis = 0;
+let elektriba_plusos = 0;
+let timeoutID;
+
+saule.addEventListener('click',() => {
+    tekts.style.transform = "scale(1.2)";
+    setTimeout(() => {
+        teksts.style.transform = "scale(1)";
+    }, 100);
+    elektriba_plusos += 1;
+    klikskis += 1;
+    teksts.textContent = '+' + klikskis;
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(() => {
+        klikskis = 0
+        tekts.textContent = ''
+        elektriba += elektriba_plusos;
+        elektriba_plusos = 0
+    }, 500);
+})
+
+setInterval(() => {
+    skaititajs.textContent = 'Elektrība = ' + elektriba;
+}, 1);
+
+
+
