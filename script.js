b4a51a1d9a063a90f9746cc8407c5037622f04d1
EMVaivods <mihaelsvaivods@gmail.com>
2026-06-02 21:40:32 +0300

    Pievienoju elektribas vertibas tabulu

--- script.js
+++ script.js
@@ -17,16 +17,28 @@ sanu_verejs.addEventListener("click", () => {
     } 
     else {
         neredzama_dala.style.right = '0px';
-        sanu_verejs.style.right = '300px'
+        sanu_verejs.style.right = '15.4vw';
     }
 });
 
 const skaititajs = document.getElementById('skaititajs')
-let elektriba = 0
+let Wh = 0
+let kWh = 0
 
 setInterval(() => {
-    elektriba += 1;
-    skaititajs.textContent = 'Elektrība = ' + elektriba;
+    kWh += 0.001;
+    Wh += 1;
+    if (kWh < 1) {
+    skaititajs.textContent = 'Energy = ' + Wh + ' Wh';
+} else if (kWh < 1000) {
+    skaititajs.textContent = 'Energy = ' + kWh.toFixed(3) + ' kWh';
+} else if (kWh < 1000000) {
+    skaititajs.textContent = 'Energy = ' + (kWh / 1000).toFixed(3) + ' MWh';
+} else if (kWh < 1000000000) {
+    skaititajs.textContent = 'Energy = ' + (kWh / 1000000).toFixed(3) + ' GWh';
+} else {
+    skaititajs.textContent = 'Energy = ' + (kWh / 1000000000).toFixed(3) + ' TWh';
+}
 }, 1000);
 
 const saule = document.getElementById('saule')
@@ -40,21 +52,58 @@ saule.addEventListener('click',() => {
     setTimeout(() => {
         teksts.style.transform = "scale(1)";
     }, 100);
-    elektriba_plusos += 1;
-    klikskis += 1;
+    elektriba_plusos += 1000;
+    klikskis += 1000;
     teksts.textContent = '+' + klikskis;
     clearTimeout(timeoutID);
     timeoutID = setTimeout(() => {
         klikskis = 0
         tekts.textContent = ''
-        elektriba += elektriba_plusos;
+        Wh += elektriba_plusos;
+        kWh += elektriba_plusos / 1000;
         elektriba_plusos = 0
     }, 500);
 })
 
-setInterval(() => {
-    skaititajs.textContent = 'Elektrība = ' + elektriba;
-}, 1);
 
 
+const upgrade_poga = document.getElementById('upgrade')
+const quest_poga = document.getElementById('quests')
+const sasniegumu_poga = document.getElementById('sasniegumi')
 
+upgrade_poga.addEventListener('click', () => {
+    upgrade_poga.style.borderColor = "#bba0b2"; 
+    upgrade_poga.style.backgroundColor = "#222e50";
+    upgrade_poga.style.color = "#bba0b2";
+    upgrade_poga.style.hover = "#a37e96";
+    quest_poga.style.borderColor = "#222e50";
+    quest_poga.style.backgroundColor = "#bba0b2";
+    quest_poga.style.color = "#222e50";
+    sasniegumu_poga.style.borderColor = "#222e50";
+    sasniegumu_poga.style.backgroundColor = "#bba0b2";
+    sasniegumu_poga.style.color = "#222e50";
+});
+
+quest_poga.addEventListener('click', () => {
+    quest_poga.style.borderColor = "#bba0b2"; 
+    quest_poga.style.backgroundColor = "#222e50";
+    quest_poga.style.color = "#bba0b2";
+    upgrade_poga.style.borderColor = "#222e50";
+    upgrade_poga.style.backgroundColor = "#bba0b2";
+    upgrade_poga.style.color = "#222e50";
+    sasniegumu_poga.style.borderColor = "#222e50";
+    sasniegumu_poga.style.backgroundColor = "#bba0b2";
+    sasniegumu_poga.style.color = "#222e50";
+});
+
+sasniegumu_poga.addEventListener('click', () => {
+    sasniegumu_poga.style.borderColor = "#bba0b2"; 
+    sasniegumu_poga.style.backgroundColor = "#222e50";
+    sasniegumu_poga.style.color = "#bba0b2";
+    upgrade_poga.style.borderColor = "#222e50";
+    upgrade_poga.style.backgroundColor = "#bba0b2";
+    upgrade_poga.style.color = "#222e50";
+    quest_poga.style.borderColor = "#222e50";
+    quest_poga.style.backgroundColor = "#bba0b2";
+    quest_poga.style.color = "#222e50";
+});
